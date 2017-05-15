19b75cc18cf4e4496c187e195d4b352e2b59f915
momus300 <momus300@gmail.com>
2017-05-16 00:36:00 +0200

    zmiany z pecka

--- src/AppBundle/Controller/PanelController.php
+++ src/AppBundle/Controller/PanelController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\QueryBuilder;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,5 +14,19 @@ class PanelController extends Controller
         $em = $this->getDoctrine()->getManager();
         $brands = $em->getRepository('AppBundle:Brands')->findAll();
         return $this->render('@App/panel/show.html.twig', ['brands' => $brands]);
+    }
+
+    public function activitiesAction(Request $request)
+    {
+        $activities = [];
+        if ($request->isMethod('post')) {
+            $param = $request->get('test');
+            $em = $this->getDoctrine()->getManager();
+
+            $qb = $em->createQueryBuilder();
+
+//            $activities = $em->getRepository('AppBundle:Activities')->findBy(['code' => $param]);
+        }
+        return $this->render('@App/panel/acrivities.html.twig', ['activities' => $activities]);
     }
 }
